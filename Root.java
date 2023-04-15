aca0edd72b4234968b04ef9da0cb913a60946536
Yuriy Chikhonadskiy <109043213+Alocard8934@users.noreply.github.com>
2023-04-15 21:04:05 +0200

    Add files via upload
    Lesson two. This program calculates the square root of a number. To calculate from the desired number, you need to change the number in the variable.

---
+++ Root.java
@@ -0,0 +1,8 @@
+package JAVA24;
+
+class Root {
+    public static void main(String[] args) {
+        int number = 336;
+        System.out.println("The square root of " + number + " equals " + Math.sqrt(number));
+    }
+}
